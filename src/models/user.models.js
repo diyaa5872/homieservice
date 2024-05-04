5053f3724c7fdcb50145cf81f434fa78d3f1dd3f
diyaa5872 <dhankhardiya.8@gmail.com>
2024-05-04 21:45:56 +0530

    dwfwqfqfwq

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import jwt from 'jsonwenytoken'
+import bcrypt from 'bcrtpt'
 
 const addressSchema = new mongoose.Schema({
     street: {
@@ -52,5 +54,16 @@ const userSchema=new Schema({
         ref: "Worker"
     }
 },{timestamps: true})
+
+userSchema.pre('save',async function(next){//here ,next means that after all the above happened then next runs and other thing is,make it like when we are sending password then only save it otherwise not when we are changing some other thing in schema
+    if(!this.isModified('password')) return next();
+
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
 
 export const User=mongoose.model("User",userSchema)
