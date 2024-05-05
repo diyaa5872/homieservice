b1c848ce7a56e52b5d06c1d2198e34193d0a57f7
diyaa5872 <dhankhardiya.8@gmail.com>
2024-05-05 21:56:36 +0530

    dwfwfw

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,82 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/ApiError.js'
+import {User} from '../models/user.models.js'
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 
 const registerUser=asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message: 'ok'
+    //get user details from frontend
+    //validation-not empty
+    //check if user already 
+    //check for images 
+    //upload them to cloudinary 
+    //create user object-create entry in db
+    //remove password field from response
+    //check for user creation
+    //return res
+
+    const {username,email,password}=req.body
+    console.log("Email:",email)
+
+    if(
+        [fullname,email,username,password].some((field)=>
+        field?.trim() === "")
+    ){
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const existedUser=User.findOne({
+        $or : [{ username },{ email }]
     })
+    console.log(existedUser);
+
+    if(existedUser){
+        throw new ApiError(409,"User with email or username already exists");
+    }
+    console.log(req.files);
+
+    const coverImageLocalPath=req.files?.cover_image?.path;
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400,'CoverImage file is required');
+    }
+
+    const cover_image=await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!cover_image){
+        throw new ApiError(400,"cover_image is required");
+    }
+
+    const user = await User.create({
+        username: username.toLowerCase(),
+        email: email,
+        password: password,
+        cover_image: cover_image?.url || "",
+        address_user: [
+            {
+                street: "street",
+                city: "city",
+                state: "state",
+                country: "country",
+                postalCode: "postalcode"
+            }
+        ],
+        otp_code: otp_code,
+        worker_id: worker_id
+    })
+
+    const createdUser=await User.findById(user._id).select(
+        "-password -refreshToken -worker_id -otp_code -address_user -cover_image"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500,"Something went wrong while registering the User")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"user registered successfully")
+    )
+
 })
 
 export {
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -52,18 +52,46 @@ const userSchema=new Schema({
     worker_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Worker"
+    },
+    refreshToken: {
+        type: String
     }
 },{timestamps: true})
 
 userSchema.pre('save',async function(next){//here ,next means that after all the above happened then next runs and other thing is,make it like when we are sending password then only save it otherwise not when we are changing some other thing in schema
     if(!this.isModified('password')) return next();
 
-    this.password=bcrypt.hash(this.password,10)
+    this.password=await bcrypt.hash(this.password,10)
     next()
 })
 
 userSchema.methods.isPasswordCorrect=async function(password){
     return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username:this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username:this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
 }
 
 export const User=mongoose.model("User",userSchema)
