ef97cfe976b9e89d3473bc170e854d632c12f562
diyaa5872 <dhankhardiya.8@gmail.com>
2024-05-16 22:44:36 +0530

    fdfwedw

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css'; // Make sure to import your CSS file
+
+const App = () => {
+  return (
+    <div id="page1">
+
+<div id="nav">
+
+ <h1>Coucher</h1>
+ <div id="nav-part2">
+ <h3>Services</h3>
+
+ <h3>About US</h3>
+
+<h3>Cases</h3>
+
+ </div>
+
+<h4>Sign up</h4>
+
+</div>
+
+<div id="container">
+
+ <div id="left">
+
+ <h1>unlimited</h1>
+
+<h1>pretendious</h1>
+
+<h1><span><img src="https://th.bing.com/th/id/OIP.033vYDcXI0ekUO6g-NLFqwHaF5?w=221&h=180&c=7&r=0&o=5&pid=1.7" alt=""></span><span><img src="https://th.bing.com/th/id/OIP.033vYDcXI0ekUO6g-NLFqwHaF5?w=221&h=180&c=7&r=0&o=5&pid=1.7" alt=""></span><span><img src="https://th.bing.com/th/id/OIP.033vYDcXI0ekUO6g-NLFqwHaF5?w=221&h=180&c=7&r=0&o=5&pid=1.7" alt=""></span><span>Ideas</span></h1>
+
+</div>
+<div id="right">
+
+<img src="https://images.unsplash.com/photo-1645732712972-6a1167c17738?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YSUyMGdpcmwlMjByZWFkaW5nJTIwYm9vayUyMHBhaW50aW5nfGVufDB8fDB8fHww" alt="">
+
+ </div>
+
+ </div>
+
+</div>
+  )
+}
+
+export default App;
